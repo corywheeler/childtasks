8cb2161f3fe3943fe2d5a638483dd3f48bf2262d
Cory Wheeler <corywheeler@gmail.com>
2016-06-28 16:50:02 -0400

    initial commit
    this uses a where clause to get defects and their Resolution for defects assigned
    to a particular project with a particular project name

---
+++ ChildTasks/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using VersionOne.SDK.APIClient;
+
+namespace ChildTasks
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Debug.Print("--- Start ---");
+
+			V1Connector connector = V1Connector
+				.WithInstanceUrl("http://localhost/VersionOne.Web")
+				.WithUserAgentHeader("ChildTasks", "1.0")
+				.WithUsernameAndPassword("admin", "admin")
+				.Build();
+
+			IServices services = new Services(connector);
+			var v1ProjectName = "Batting Order";
+			var passThroughQuery = "{" +
+			                       "  \"from\": \"Defect\"," +
+			                       "  \"select\": [\"Name\",\"Description\",\"Resolution\",\"ResolutionReason.Name\",\"Children:Task.Name\", \"Children:Task.Description\"]," +
+								   "  \"where\": {\"Scope.Name\" : " + v1ProjectName + "} " +
+			                       "}";
+
+			var result = services.ExecutePassThroughQuery(passThroughQuery);
+
+			Debug.Print("Here it is:" + result);
+			
+			
+			Debug.Print("--- End ---");
+		}
+	}
+}
