0e7008b3ab4fd8089925e79a7b64a0bbac72dba3
Cory Wheeler <corywheeler@gmail.com>
2016-07-01 13:59:27 -0400

    some stuff

--- ChildTasks/Program.cs
+++ ChildTasks/Program.cs
@@ -17,11 +17,67 @@ namespace ChildTasks
 
 			IServices services = new Services(connector);
 			var v1ProjectName = "Batting Order";
-			var passThroughQuery = "{" +
-			                       "  \"from\": \"Defect\"," +
-			                       "  \"select\": [\"Name\",\"Description\",\"Resolution\",\"ResolutionReason.Name\",\"Children:Task.Name\", \"Children:Task.Description\"]," +
-								   "  \"where\": {\"Scope.Name\" : " + v1ProjectName + "} " +
-			                       "}";
+			var v1Sprint = "Iteration 1";
+//			var passThroughQuery = @"{ 
+//""from"": ""Defect"",
+//""select"": [""Name"",""Description"",""Resolution"",""ResolutionReason.Name"",""Children:Task.Name"", ""Children:Task.Description""],
+//""where"": {
+//	""Children:Task.Name"" : ""Release Notes"",
+//	""Scope.Name"" : " + v1ProjectName + @"}
+//}";
+
+			var passThroughQuery = @"
+from: Defect
+select: 
+  - Name
+  - Number
+  - Description
+  - Resolution
+  - ResolutionReason.Name
+  - from: Children:Task
+    select:
+      - Name
+      - Description
+    where: 
+      Name: Release Notes
+where:
+  Scope.Name: " + v1ProjectName + @"
+  Timebox.Name: " + v1Sprint;
+
+
+//var passThroughQuery = @"
+//from: Defect
+//select: 
+//  - Name
+//  - Number
+//  - Description
+//  - Resolution
+//  - ResolutionReason.Name
+//  - from: Children:Task
+//    select:
+//      - Name
+//      - Description
+//    filter:
+//      - Name!='Release Notes'
+//where:
+//  Scope.Name: " + v1ProjectName + @"
+//  Timebox.Name: " + v1Sprint;
+
+//			var passThroughQuery = @"
+//from: Defect
+//select: 
+//  - Name
+//  - Number
+//  - Description
+//  - Resolution
+//  - ResolutionReason.Name
+//where:
+//  Scope.Name: " + v1ProjectName + @"
+//  Timebox.Name: " + v1Sprint + @"
+//filter:
+//  - Number!='D-01001'
+//";
+
 
 			var result = services.ExecutePassThroughQuery(passThroughQuery);
 
